71e5486f4da066dd630db9d6e8e70735ed1a88a5
Raahiltfb <raahil.mehta003@gmail.com>
2026-02-07 17:23:34 +0530

    Update Contact.tsx

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -371,7 +371,7 @@ const Contact = () => {
             <div className="card-industrial rounded-lg overflow-hidden">
               <div className="aspect-[21/9] w-full">
                 <iframe
-                  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3684.0989372545894!2d88.36282647595314!3d22.578946933689724!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3a0277a6e8e98cc7%3A0xa8dfcb5a74946fd8!2sWestern%20Consolidated%20Pvt.%20Ltd.!5e0!3m2!1sen!2sin!4v1709123456789!5m2!1sen!2sin"
+                  src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d7368.549507369673!2d88.354976!3d22.568825!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3a0277a91a13a94f%3A0xf2368156fc970667!2sWestern%20Consolidated%20Private%20Limited!5e0!3m2!1sen!2sus!4v1770465156684!5m2!1sen!2sus"
                   width="100%"
                   height="100%"
                   style={{ border: 0 }}
