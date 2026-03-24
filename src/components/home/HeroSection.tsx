123462a33a8b5cbd1a78f706a1f0286893015a83
Raahiltfb <raahil.mehta003@gmail.com>
2026-03-24 12:54:51 +0530

    testing bgagain

--- src/components/home/HeroSection.tsx
+++ src/components/home/HeroSection.tsx
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom';
 import { Button } from '@/components/ui/button';
 import kirloskarLogo from '@/assets/kirloskar-logo.png';
 import heroGenerator from '@/assets/hero-generator.png';
-import heroBg from '@/assets/anotherbg.jpg';
+import heroBg from '@/assets/bgagain.jpg';
 
 interface HeroSectionProps {
   onAnimationComplete: () => void;
