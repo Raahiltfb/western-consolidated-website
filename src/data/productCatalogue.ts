a9cbcdbb5c482865eb670d42cc4f18d00d1e38a3
Raahiltfb <raahil.mehta003@gmail.com>
2026-03-26 18:35:27 +0530

    engine type modified

--- src/data/productCatalogue.ts
+++ src/data/productCatalogue.ts
@@ -32,7 +32,7 @@ const defaultHighlights = [
 
 const createSpecifications = (powerRating: string, fuel: string = 'Diesel') => [
   { label: 'Power Rating', value: powerRating },
-  { label: 'Engine', value: 'WCPL' },
+  { label: 'Engine', value: 'KOEL' },
   { label: 'Frequency', value: '50 Hz' },
   { label: 'Voltage', value: '415V / 240V' },
   { label: 'Fuel', value: fuel },
